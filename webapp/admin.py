ca5a7bfbf27139b23f63040e2fa06d64a6788ecc
Ahmed Elmarghany <amelmarghany@gmail.com>
2026-01-16 08:51:44 +0200

    set up app models

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Record
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    pass
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+# Client model
+class Record(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    phone = models.IntegerField()
+    tall = models.IntegerField()
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    address = models.CharField(max_length=500)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
