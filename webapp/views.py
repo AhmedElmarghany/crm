3b05738fda7144d8556a3a5f385174a40aef4409
Ahmed Elmarghany <amelmarghany@gmail.com>
2026-01-16 09:13:12 +0200

    feat(auth): add register form including form and template

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .forms import CreateUserForm
 
 def index(request):
     return render(request, 'web/index.html')
+
+def register(request):
+    form = CreateUserForm(request.POST)
+    if request.method == 'POST':
+        form.save()
+    else:
+        form = CreateUserForm()
+    
+    context = {'form': form}
+
+    return render(request, 'web/register.html', context)
